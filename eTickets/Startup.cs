93b29d4c1fdf01bb9bea5d0d438cd14b509b1be2
Inwtn <lutevl12@gmail.com>
2025-02-24 23:53:57 -0300

    correcao

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,6 +1,55 @@
-﻿namespace eTickets
+﻿
+using eTickets.Data;
+
+namespace eTickets
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //DbContext configuration
+            services.AddDbContext<AppDbContext>();
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession();
+
+            //Authentication & Authorization
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
+            });
+        }
     }
 }
